9961dc3d4902cba57d61ece2bf27b83d6c34ca7a
Ethan Owen <ethan@eocreative.dev>
2026-03-12 17:32:03 +0000

    Set UI Title for work-orders screen

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -34,6 +34,7 @@ export default function AppLayout() {
 			<Tabs.Screen name="dashboard" options={{ title: "Dashboard" }} />
 			<Tabs.Screen name="profile" options={{ title: "Profile" }} />
 			<Tabs.Screen name="settings" options={{ title: "Settings" }} />
+			<Tabs.Screen name="work-orders" options={{ title: "Work Orders" }} />
 		</Tabs>
 	);
 }
