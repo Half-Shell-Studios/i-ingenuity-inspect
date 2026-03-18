e9555c3227bb5ce1eff4fd67e3799b874bb8e491
Ethan Owen <ethan@eocreative.dev>
2026-03-18 20:01:44 +0000

    Import schema definitions to the root drizzle config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
+import * as schema from "@/src/db/schema/";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import * as SQLite from "expo-sqlite";
-import * as schema from './schemas/workOrder';
 
 // Opens the existing downloaded DB — does NOT create tables
 export function getDb( dbName: string ) {
