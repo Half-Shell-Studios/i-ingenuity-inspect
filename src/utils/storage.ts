b2bcb6e3522355b71839e658d5dd372e284d6ed1
Ethan Owen <ethan@eocreative.dev>
2026-03-26 18:26:30 +0000

    Create methods to get, set and remove the active Work Order UUID from the SecureStore

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,7 +1,8 @@
 import * as SecureStore from "expo-secure-store";
 
-const TOKEN_KEY = "auth_token";
-const LAST_ROUTE_KEY = "last_route";
+const TOKEN_KEY = 'auth_token';
+const LAST_ROUTE_KEY = 'last_route';
+const ACTIVE_WORK_ORDER_KEY = 'active_work_order_uuid';
 
 export const setToken = (token: string) => SecureStore.setItemAsync( TOKEN_KEY, token );
 
@@ -14,3 +15,9 @@ export const setLastRoute = (route: string) => SecureStore.setItemAsync( LAST_RO
 export const getLastRoute = () => SecureStore.getItemAsync( LAST_ROUTE_KEY );
 
 export const removeLastRoute = () => SecureStore.deleteItemAsync( LAST_ROUTE_KEY );
+
+export const setActiveWorkOrderUuid = (uuid: string) => SecureStore.setItemAsync( ACTIVE_WORK_ORDER_KEY, uuid );
+
+export const getActiveWorkOrderUuid = () => SecureStore.getItemAsync( ACTIVE_WORK_ORDER_KEY );
+
+export const removeActiveWorkOrderUuid = () => SecureStore.deleteItemAsync( ACTIVE_WORK_ORDER_KEY );
