53e054c7aa14e6ee18cc7f0852a7a47f8d74ad97
Ethan Owen <ethan@eocreative.dev>
2026-03-26 18:43:28 +0000

    Remove hard coded DB url from base config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config';
+// import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
 	out: './drizzle',
 	schema: './src/db/schema',
 	dialect: 'sqlite',
-	dbCredentials: {
-		// url: process.env.DB_FILE_NAME!,
-		url: 'work_orders.db'
-	},
+	// dbCredentials: {
+	// 	url: process.env.DB_FILE_NAME!,
+	// 	url: 'work_orders.db'
+	// },
 });
