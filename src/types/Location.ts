6712d26736136e3682f794a058673f0b26d99e97
Ethan Owen <ethan@eocreative.dev>
2026-04-02 18:23:07 +0100

    Allow undefined location names to allow grouping by each level

--- src/types/Location.ts
+++ src/types/Location.ts
@@ -1,9 +1,9 @@
 export interface Location {
 	id: string,
-	customerName: string,
-	siteName: string,
-	plantName: string,
-	areaName: string
+	customerName?: string,
+	siteName?: string,
+	plantName?: string,
+	areaName?: string
 }
 
 export default Location;
