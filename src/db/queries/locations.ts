8147a83fba35babba7e45987baa9b836e0d8acc2
Ethan Owen <ethan@eocreative.dev>
2026-04-02 18:22:26 +0100

    Create DB queries for locations

---
+++ src/db/queries/locations.ts
@@ -0,0 +1,33 @@
+import { locationsTable } from "@/src/db/schema";
+import { sql } from "drizzle-orm";
+import { AppDatabase } from "..";
+
+export async function getAllLocations( db: AppDatabase | null ) {	
+	if( !db ) return;
+	
+	return db.select().from( locationsTable ).all();
+}
+
+export async function getAllCutsomers( db: AppDatabase | null ) {
+	if( !db ) return;
+	
+	return db.select({ id: sql<string>`min(${locationsTable.id})`, customerName: locationsTable.customerName }).from( locationsTable ).groupBy( locationsTable.customerName ).orderBy( locationsTable.customerName ).all();
+}
+
+export async function getAllSites( db: AppDatabase | null ) {
+	if( !db ) return;
+	
+	return db.select({ id: sql<string>`min(${locationsTable.id})`, siteName: locationsTable.siteName }).from( locationsTable ).groupBy( locationsTable.siteName ).orderBy( locationsTable.siteName ).all();
+}
+
+export async function getAllPlants( db: AppDatabase | null ) {
+	if( !db ) return;
+	
+	return db.select({ id: sql<string>`min(${locationsTable.id})`, plantName: locationsTable.plantName }).from( locationsTable ).groupBy( locationsTable.plantName ).orderBy( locationsTable.plantName ).all();
+}
+
+export async function getAllAreas( db: AppDatabase | null ) {
+	if( !db ) return;
+	
+	return db.select({ id: sql<string>`min(${locationsTable.id})`, areaName: locationsTable.areaName }).from( locationsTable ).groupBy( locationsTable.areaName ).orderBy( locationsTable.areaName ).all();
+}
