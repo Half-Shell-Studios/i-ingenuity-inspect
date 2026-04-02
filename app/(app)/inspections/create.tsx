c47e06716afcb5639ebf78674f858dd0e59b77a2
Ethan Owen <ethan@eocreative.dev>
2026-04-02 18:27:27 +0100

    Create a 'create an inspection' screen

--- app/(app)/inspections/create.tsx
+++ app/(app)/inspections/create.tsx
@@ -1,10 +1,42 @@
-import { Text, View } from "react-native"
+import ScrollViewContainer from "@/src/components/ScrollViewContainer";
+import { useWorkOrderDb } from "@/src/context/WorkOrderDbContext";
+import { getAssetTagById } from "@/src/db/queries/assetTags";
+import { getAllInspectionTypes } from "@/src/db/queries/inspections";
+import { AssetTag, InspectionType } from "@/src/types/";
+import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
 
 function CreateInspection() {
+	const { assetTag: assetTagId } = useLocalSearchParams<{ assetTag: string }>();
+	const [ inspectionTypes, setInspectionTypes ] = useState<InspectionType[]>([]);
+	const [ assetTag, setAssetTag ] = useState<AssetTag>();
+	const { isReady: dbIsReady, error: dbError, db } = useWorkOrderDb();
+	
+	useEffect(() => {
+		if( dbIsReady ) {
+			(async() => {
+				setInspectionTypes( await getAllInspectionTypes( db ) )
+			})();
+			(async() => {
+				setAssetTag( await getAssetTagById( db, assetTagId ) )
+			})();
+		}
+	}, [ db, dbIsReady, assetTagId ]);
+
+	if( dbError ) return <Text>Error: { dbError }</Text>;
+	if( !dbIsReady || !db ) return <ActivityIndicator />;
+
 	return (
-		<View>
-			<Text>CreateInspection</Text>
-		</View>
+		<ScrollViewContainer>
+			<Text>Create an Inspection for { assetTag?.name }</Text>
+			<Text>Choose your Inspection Type</Text>
+			{inspectionTypes.map( inspectionType => (
+				<View key={ inspectionType.id }>
+					<Text>{ inspectionType.name }</Text>
+				</View>
+			))}
+		</ScrollViewContainer>
 	)
 }
 
