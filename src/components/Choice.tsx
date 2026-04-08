2761533091f703576f742fb8fd25821cb4da93bb
Ethan Owen <ethan@eocreative.dev>
2026-04-08 11:56:25 +0100

    Create a reusable component for radio style input choices

---
+++ src/components/Choice.tsx
@@ -0,0 +1,41 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Choice({ label, value, selected }: { label: string, value: string, selected: boolean }) {
+	return (
+		<View style={ styles.container }>
+			<Text>{ label }</Text>
+			{ selected ? (
+				<Ionicons name="checkmark-circle" size={24} color="#7863FB" />
+			) : (
+				<Ionicons name="checkmark-circle-outline" size={24} color="#C0C0C0" />
+			)}
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		padding: 10,
+		backgroundColor: "#f9fafb",
+		borderRadius: 12,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 1
+		},
+		shadowOpacity: 0.1,
+		shadowRadius: 3,
+		marginBottom: 10,
+	},
+	label: {
+		flex: 1
+	},
+	icon: {
+		flex: 0
+	}
+})
