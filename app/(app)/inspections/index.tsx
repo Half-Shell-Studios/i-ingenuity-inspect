2e5e6afdb1907fadc5e51dfe1f360ca3499542f7
Ethan Owen <ethan@eocreative.dev>
2026-04-09 15:21:22 +0100

    Set up inspections index screen

--- app/(app)/inspections/index.tsx
+++ app/(app)/inspections/index.tsx
@@ -1,10 +1,17 @@
+import Card from "@/src/components/Card";
+import CardsContainer from "@/src/components/CardsContainer";
+import CardTitle from "@/src/components/CardTitle";
+import ScreenTitle from "@/src/components/ScreenTitle";
+import ScrollViewContainer from "@/src/components/ScrollViewContainer";
 import { useWorkOrderDb } from "@/src/context/WorkOrderDbContext";
 import { getAllInspections } from "@/src/db/queries/inspections";
 import { Inspection } from "@/src/types";
+import { useRouter } from "expo-router";
 import { useEffect, useState } from "react";
-import { ActivityIndicator, Text, View } from "react-native";
+import { ActivityIndicator, Text, TouchableOpacity } from "react-native";
 
 function Inspections() {
+	const router = useRouter();
 	const { isReady: dbIsReady, error: dbError, db } = useWorkOrderDb();
 	const [ inspections, setInspections ] = useState<Inspection[]>([]);
 
@@ -20,15 +27,28 @@ function Inspections() {
 	if( !dbIsReady || !db ) return <ActivityIndicator />;
 
 	return (
-		<View>
-			<Text>Inspections</Text>
-			{inspections.map( inspection => (
-				<View key={ inspection.id }>
-					<Text>{ inspection.name }</Text>
-				</View>
-			))}
-		</View>
-	)
+		<ScrollViewContainer>
+			<ScreenTitle title="Inspections" />
+			{ inspections?.length && (
+				<CardsContainer>
+					{inspections.map( inspection => {
+						return (
+							<TouchableOpacity key={ inspection?.id } onPress={() => router.push({
+								pathname: "/inspections/[inspection]",
+								params: {
+									inspection: inspection?.id
+								}
+							})}>
+								<Card>
+									<CardTitle title={ inspection?.name } />
+								</Card>
+							</TouchableOpacity>
+						)
+					})}
+				</CardsContainer>
+			)}
+		</ScrollViewContainer>
+	);
 }
 
 export default Inspections
