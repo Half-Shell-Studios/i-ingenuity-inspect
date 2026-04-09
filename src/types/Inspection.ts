a6381ffcc89c22fcc5116b54c207414c70df7edc
Ethan Owen <ethan@eocreative.dev>
2026-04-09 14:04:29 +0100

    Set up type interfaces for InpsectionSection and InspectionAnswer

--- src/types/Inspection.ts
+++ src/types/Inspection.ts
@@ -1,9 +1,21 @@
+type InspectionAnswer = {
+	pass: boolean;
+	notes: string;
+	value: string;
+	faults: string[];
+};
+
+export interface InspectionAnswers {
+	answer: InspectionAnswer,
+	skipped: boolean
+}
+
 export interface Inspection {
 	id: string,
 	assetTagId: string,
 	inspectionTemplateId: string,
 	name: string,
-	answers: string | null,
+	assessment: InspectionAnswers[],
 	notes: string | null,
 	inspectedBy: string
 }
