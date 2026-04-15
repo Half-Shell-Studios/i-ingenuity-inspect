0a048006ac58cf323988842323afc4059a596875
Ethan Owen <ethan@eocreative.dev>
2026-04-15 14:01:55 +0100

    Define relationship with revisions

--- src/db/schema/inspectionTemplates.ts
+++ src/db/schema/inspectionTemplates.ts
@@ -1,7 +1,20 @@
+import { relations } from "drizzle-orm";
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import inspectionTemplateRevisionsTable from "./inspectionTemplateRevisions";
 
 export const inspectionTemplatesTable = sqliteTable( 'inspection_templates', {
-	id: text( 'id' ).primaryKey(),
-	revisionActiveId: text( 'revision_active_id' ),
-	revisionLatestId: text( 'revision_latest_id' ),
+	id: text( 'id' ).primaryKey().notNull(),
+	revisionActiveId: text( 'revision_active_id' ).notNull(),
+	revisionLatestId: text( 'revision_latest_id' ).notNull(),
 });
+
+export const inspectionTemplatesRelations = relations(
+	inspectionTemplatesTable, ({ one }) => ({
+		revisionActive: one(
+			inspectionTemplateRevisionsTable, {
+				fields: [ inspectionTemplatesTable.revisionActiveId ],
+				references: [ inspectionTemplateRevisionsTable.id ],
+			}
+		),
+	})
+);
