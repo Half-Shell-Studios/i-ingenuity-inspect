c2857819151da1d8bd33a57919dace845c4d741c
Ethan Owen <ethan@eocreative.dev>
2026-04-15 17:01:29 +0100

    Add typing to createNewInspection. Set up methods to load inspectionTemplates

--- src/db/queries/inspections.ts
+++ src/db/queries/inspections.ts
@@ -1,11 +1,34 @@
-import { inspectionsTable, inspectionTypesTable } from "@/src/db/schema";
+import { inspectionsTable, inspectionTemplateRevisionsTable, inspectionTemplatesTable, inspectionTypesTable } from "@/src/db/schema";
 import { eq } from "drizzle-orm";
 import { AppDatabase } from "..";
+import { Inspection } from "@/src/types";
 
 export async function getAllInspections( db: AppDatabase | null ) {
 	if( !db ) return [];
 	
 	return db.select().from( inspectionsTable ).all();
+}
+
+export async function getInspectionById( db: AppDatabase | null, id: string ) {
+	if( !db ) return;
+	
+	return db.select().from( inspectionsTable ).where(
+		eq( inspectionsTable.id, id )
+	).get();
+}
+
+export async function getInspectionsByAssetTag( db: AppDatabase | null, assetTagId: string ) {
+	if( !db ) return;
+	
+	return db.select().from( inspectionsTable ).where(
+		eq( inspectionsTable.assetTagId, assetTagId )
+	).all();
+}
+
+export async function createNewInspection( db: AppDatabase | null, data: Omit<Inspection, "id"> ) {
+	if( !db ) return;
+
+	return await db.insert( inspectionsTable ).values( data ).returning({ insertedId: inspectionsTable.id });
 }
 
 export async function getAllInspectionTypes( db: AppDatabase | null ) {
@@ -14,27 +37,32 @@ export async function getAllInspectionTypes( db: AppDatabase | null ) {
 	return db.select().from( inspectionTypesTable ).all();
 }
 
-export async function getInspectionById( db: AppDatabase | null, id: string) {
-	if( !db ) return;
-	
-	return db.select().from( inspectionsTable ).where(
-		eq( inspectionsTable.id, id )
-	).get();
-}
-
-export async function getInspectionsByAssetTag( db: AppDatabase | null, assetTagId: string) {
-	if( !db ) return;
-	
-	return db.select().from( inspectionsTable ).where(
-		eq( inspectionsTable.assetTagId, assetTagId )
-	).all();
-}
-
-export async function createNewInspection( db: AppDatabase | null, tagId: string, inspectionTypeId: string ) {
+export async function getInspectionTemplateById( db: AppDatabase | null, inspectionTemplateId: string ) {
 	if( !db ) return;
 
-	return await db.insert( inspectionsTable ).values({
-		assetTagId: tagId,
-		inspectionTemplateId: inspectionTypeId
+	return db.query.inspectionTemplatesTable.findFirst({
+		where: eq( inspectionTemplatesTable.id, inspectionTemplateId ),
+		with: {
+			revisionActive: true
+		}
 	});
+}
+
+export async function getInspectionTemplateByRevisionId( db: AppDatabase | null, inspectionTemplateRevisionId: string ) {
+	if( !db ) return;
+
+	return db.query.inspectionTemplatesTable.findFirst({
+		where: eq( inspectionTemplatesTable.revisionActiveId, inspectionTemplateRevisionId ),
+		with: {
+			revisionActive: true
+		}
+	});
+}
+
+export async function getInspectionTemplatesByInspectionType( db: AppDatabase | null, inspectionTypeId: string ) {
+	if( !db ) return [];
+
+	return db.select().from( inspectionTemplateRevisionsTable ).where(
+		eq( inspectionTemplateRevisionsTable.inspectionTypeId, inspectionTypeId )
+	).all();
 }
