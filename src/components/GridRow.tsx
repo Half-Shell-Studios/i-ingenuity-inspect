ee3cef7708470ba7a6179260b2868165f84c8fac
Ethan Owen <ethan@eocreative.dev>
2026-04-16 11:11:30 +0100

    Set grid row styles

--- src/components/GridRow.tsx
+++ src/components/GridRow.tsx
@@ -1,12 +1,22 @@
 import { ReactNode } from "react"
-import { View } from "react-native"
+import { StyleSheet, View } from "react-native"
 
 function GridRow({ children }: { children: ReactNode }) {
 	return (
-		<View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
+		<View style={ styles.row }>
 			{ children }
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	row: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		alignItems: 'center',
+		gap: 10,
+		marginBottom: 20
+	}
+})
 
 export default GridRow
