932d0f69c34e78d6aa79257eb4e48c5f760308a3
Ethan Owen <ethan@eocreative.dev>
2026-05-28 17:44:14 +0100

    Set up selectable colour options

--- src/components/TouchableOpacityButton.tsx
+++ src/components/TouchableOpacityButton.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ActivityIndicator, GestureResponderEvent, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-export default function TouchableOpacityButton({ label, pressHandler, activity = false }: { label: string, pressHandler: (event: GestureResponderEvent) => void; activity?: boolean }) {
-	const [ showsActivity, setShowsActivity ] = useState<boolean>(false);
+type Colour = 'primary' | 'error' | 'success';
 
-	useEffect(() => {
-		setShowsActivity( activity );
-	}, [ activity ]);
+const colourMap: Record<Colour, string> = {
+	primary: '#7863FB',
+	error: '#E5484D',
+	success: '#30A46C',
+};
 
+export default function TouchableOpacityButton({ label, pressHandler, activity = false, colour = 'primary' }: { label: string, pressHandler: (event: GestureResponderEvent) => void; activity?: boolean, colour?: Colour }) {
 	return (
-		<TouchableOpacity style={ styles.button } onPress={ pressHandler }>
+		// <TouchableOpacity style={ styles.button } onPress={ pressHandler }>
+		<TouchableOpacity style={[styles.button, { backgroundColor: colourMap[colour] }]} onPress={ pressHandler }>
 			<Text style={ styles.buttonText }>
-				{ showsActivity ? <ActivityIndicator color="#fff" /> : label }
+				{ activity ? <ActivityIndicator color="#fff" /> : label }
 			</Text>
 		</TouchableOpacity>
 	)
@@ -19,6 +22,7 @@ export default function TouchableOpacityButton({ label, pressHandler, activity =
 
 const styles = StyleSheet.create({
 	button: {
+		alignItems: "center",
 		paddingBlock: 8,
 		paddingInline: 16,
 		backgroundColor: "#7863FB",
