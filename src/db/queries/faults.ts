fa0ba9c579a7041a5bc0d0de1df04079255f269e
Ethan Owen <ethan@eocreative.dev>
2026-05-28 17:38:30 +0100

    Return the updated faults

--- src/db/queries/faults.ts
+++ src/db/queries/faults.ts
@@ -20,5 +20,5 @@ export async function closeFault( db: AppDatabase | null, faultId: string, userI
 		closedComment: comment,
 	}).where(
 		eq( faultsTable.id, faultId )
-	).run();
+	).returning();
 }
