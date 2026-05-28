601fc8b0114864aa70178f89e1245807dbaa62a1
Ethan Owen <ethan@eocreative.dev>
2026-05-28 14:45:43 +0100

    Add an activity indicator to the button

--- src/components/TouchableOpacityButton.tsx
+++ src/components/TouchableOpacityButton.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { GestureResponderEvent, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, GestureResponderEvent, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-export default function TouchableOpacityButton({ label, pressHandler }: { label: string,   pressHandler: (event: GestureResponderEvent) => void; }) {
+export default function TouchableOpacityButton({ label, pressHandler, activity = false }: { label: string, pressHandler: (event: GestureResponderEvent) => void; activity?: boolean }) {
+	const [ showsActivity, setShowsActivity ] = useState<boolean>(false);
+
+	useEffect(() => {
+		setShowsActivity( activity );
+	}, [ activity ]);
+
 	return (
 		<TouchableOpacity style={ styles.button } onPress={ pressHandler }>
 			<Text style={ styles.buttonText }>
-				{ label }
+				{ showsActivity ? <ActivityIndicator color="#fff" /> : label }
 			</Text>
 		</TouchableOpacity>
 	)
