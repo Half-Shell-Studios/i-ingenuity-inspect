26c2c4c30a30101d018d0918648972ac123574b3
Ethan Owen <ethan@eocreative.dev>
2026-06-04 14:23:23 +0100

    Set default screen options. Remove screens now moved into tabs

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -2,6 +2,20 @@ import { setLastRoute } from '@/src/utils/storage';
 import { Stack, usePathname, useRouter } from 'expo-router';
 import { useEffect } from 'react';
 
+export function stackOptions( defaultTitle: string ) {
+	return {
+		title: defaultTitle,
+		headerShown: false,
+	}
+}
+
+export function tabOptions( defaultTitle: string ) {
+	return {
+		...stackOptions( defaultTitle ),
+		tabBarActiveTintColor: "#7863FB",
+	}
+}
+
 export default function AppLayout() {
 	const router = useRouter();
 	const pathname = usePathname();
@@ -19,32 +33,18 @@ export default function AppLayout() {
 				title: "Dashboard"
 			}} />
 			<Stack.Screen name="work-orders/index" options={{
-				title: "Work Orders",
+				title: "Work Orders"
 			}} />
 			<Stack.Screen name="work-orders/[workOrder]" options={{
-				title: "Work Order Details",
+				title: 'Work Order Details'
 			}} />
-			<Stack.Screen name="asset-tags/index" options={{
-				title: "Asset Tags",
-			}} />
-			<Stack.Screen name="asset-tags/[assetTag]" options={{
-				title: "Asset Tag Details",
-			}} />
-			<Stack.Screen name="inspections/index" options={{
-				title: "Inspections",
-			}} />
-			<Stack.Screen name="inspections/create" options={{
-				title: "Inspect a Tag",
-			}} />
-			<Stack.Screen name="inspections/[inspection]/edit" options={{
-				title: "Inspect a Tag",
-			}} />
-			<Stack.Screen name="inspections/[inspection]/index" options={{
-				title: "Inspection Details",
-			}} />
-			<Stack.Screen name="faults/[fault]" options={{
-				title: "Close a Fault",
-			}} />
+			{/* <Stack.Screen name="asset-tags/index" options={ stackOptions( 'Asset Tags' ) } /> */}
+			{/* <Stack.Screen name="asset-tags/[assetTag]" options={ stackOptions( 'Asset Tag Details' ) } /> */}
+			{/* <Stack.Screen name="inspections/index" options={ stackOptions( 'Inspections' ) } /> */}
+			{/* <Stack.Screen name="inspections/create" options={ stackOptions( 'Inspect a Tag' ) } /> */}
+			{/* <Stack.Screen name="inspections/[inspection]/edit" options={ stackOptions( 'Inspect a Tag' ) } /> */}
+			{/* <Stack.Screen name="inspections/[inspection]/index" options={ stackOptions( 'Inspection Details' ) } /> */}
+			{/* <Stack.Screen name="faults/[fault]" options={ stackOptions( 'Close a Fault' ) } /> */}
 		</Stack>
 	);
 }
