b581ffa84a1ef69a15aee08e780cea70c515e529
Ethan Owen <ethan@eocreative.dev>
2026-06-05 19:09:44 +0100

    Set up brand colours to use throughout the app's styles

---
+++ src/constants/colours.ts
@@ -0,0 +1,13 @@
+import { Appearance } from "react-native";
+
+export const BRAND_COLOUR_BLUE = '#2500fd';
+export const BRAND_COLOUR_YELLOW = '#fff006';
+export const BRAND_COLOUR_NAVY = '#151732';
+export const BRAND_COLOUR_GREY = '#e5e5e5';
+export const BRAND_COLOUR_OFFWHITE = '#f5f5f5';
+export const BRAND_COLOUR_WHITE = '#fffff';
+
+export const PRIMARY_COLOUR = BRAND_COLOUR_BLUE;
+export const SECONDARY_COLOUR = BRAND_COLOUR_YELLOW;
+
+export const darkModeActive = ( Appearance.getColorScheme() === 'dark' );
