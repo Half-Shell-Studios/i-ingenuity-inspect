23f2bef31305b5764375f07b7cd2d9c4188f49c5
Ethan Owen <ethan@eocreative.dev>
2026-06-05 19:12:10 +0100

    Set the default and auth routes as shared constant values

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
-import { useAuth } from "@/src/context/AuthContext";
-import { Redirect } from "expo-router";
+import { AUTH_ROUTE, DEFAULT_ROUTE } from '@/src/constants/routes';
+import { useAuth } from '@/src/context/AuthContext';
+import { Redirect } from 'expo-router';
 
 export default function Index() {
 	const { isAuthenticated } = useAuth();
 
-	return isAuthenticated ? <Redirect href="/(app)/dashboard" /> : <Redirect href="/(auth)/login" />;
+	return isAuthenticated ? <Redirect href={ DEFAULT_ROUTE } /> : <Redirect href={ AUTH_ROUTE } />;
 }
