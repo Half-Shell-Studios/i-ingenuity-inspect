406a305f6aa8bbb959b0d5b5ef8a4abe40470d42
Ethan Owen <ethan@eocreative.dev>
2026-06-10 15:35:33 +0100

    Fix location filters display

--- app/(app)/work-orders/[workOrder]/asset-tags/index.tsx
+++ app/(app)/work-orders/[workOrder]/asset-tags/index.tsx
@@ -1,12 +1,22 @@
+import AreasIcon from '@/assets/icons/areas.svg';
+import AssetTagIcon from '@/assets/icons/asset-tags.svg';
+import CustomersIcon from '@/assets/icons/customers.svg';
+import PlantsIcon from '@/assets/icons/plants.svg';
+import SitesIcon from '@/assets/icons/sites.svg';
 import Card from "@/src/components/Card";
 import CardsContainer from "@/src/components/CardsContainer";
+import CardTitle from "@/src/components/CardTitle";
 import GridColumn from "@/src/components/GridColumn";
 import GridRow from "@/src/components/GridRow";
+import LocationFilter from '@/src/components/LocationFilter';
 import ScreenTitle from "@/src/components/ScreenTitle";
+import TouchableOpacityButton from "@/src/components/TouchableOpacityButton";
+import { ACCENT_COLOUR } from "@/src/constants/colours";
 import { useWorkOrderDb } from "@/src/context/WorkOrderDbContext";
 import { getAllAssetTags } from "@/src/db/queries/assetTags";
+import * as locationsQuery from "@/src/db/queries/locations";
+import type { Area, Customer, Plant, Site } from "@/src/types";
 import AssetTag from "@/src/types/AssetTag";
-import { Column, Host, Picker } from '@expo/ui';
 import { useRouter } from "expo-router";
 import { Fragment, useEffect, useState } from "react";
 import { ActivityIndicator, Dimensions, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
@@ -19,38 +29,29 @@ const FLAVOURS = [
 	{ label: 'Strawberry', value: 'strawberry' },
 ];
 
-function PickerWheelExample() {
-	const [ value, setValue ] = useState( 'chocolate' );
-
-	return (
-		<Host style={{ flex: 1 }}>
-			<Column spacing={8} style={{ padding: 16 }}>
-				<Picker selectedValue={ value } onValueChange={ setValue }>
-					{FLAVOURS.map(f => (
-						<Picker.Item key={f.value} label={f.label} value={f.value} />
-					))}
-				</Picker>
-			</Column>
-		</Host>
-	);
-}
-
-
 export default function AssetTagsIndex() {
 	const router = useRouter();
 	const [ tags, setTags ] = useState<AssetTag[]>([]);
+	const [ customers, setCustomers ] = useState<Customer[]>()
+	const [ sites, setSites ] = useState<Site[]>()
+	const [ plants, setPlants ] = useState<Plant[]>()
+	const [ areas, setAreas ] = useState<Area[]>()
 	const { isReady: dbIsReady, error: dbError, db } = useWorkOrderDb();
 
 	useEffect(() => {
 		if( dbIsReady ) {	
 			(async() => {
 				setTags( await getAllAssetTags( db ) );
+				setCustomers( await locationsQuery.getAllCutsomers( db ) );
+				setSites( await locationsQuery.getAllSites( db ) );
+				setPlants( await locationsQuery.getAllPlants( db ) );
+				setAreas( await locationsQuery.getAllAreas( db ) );
 			})();
 		}
 	}, [ db, dbIsReady ]);
-
 	
 	if( dbError ) return <Text>Error: { dbError }</Text>;
+
 	if( !dbIsReady || !db || ( dbIsReady && ( tags?.length < 1 ) ) ) return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 			<ActivityIndicator />
@@ -63,23 +64,62 @@ export default function AssetTagsIndex() {
 			<View style={ styles.filtersContainer }>
 				<CardsContainer>
 					<Card>
-						<GridRow>
-							<GridColumn styles={ styles.filtersColumn }>
-								<View style={{ backgroundColor: "#F00", padding: 10 }}>
-									<Text>Customer Filter</Text>
-								</View>
-							</GridColumn>
-							<GridColumn styles={ styles.filtersColumn }>
-								<Text>Site Filter</Text>
-								<PickerWheelExample />
-							</GridColumn>
-							<GridColumn styles={ styles.filtersColumn }>
-								<Text>Plant Filter</Text>
-							</GridColumn>
-							<GridColumn styles={ styles.filtersColumn }>
-								<Text>Area Filter</Text>
-							</GridColumn>
-						</GridRow>
+						<CardTitle title="Filters" />
+						<View style={{ marginInline: -5 }}>
+							<GridRow>
+								<GridColumn style={ styles.filtersColumn }>
+									<GridRow>
+										<GridColumn style={ styles.iconContainer }>
+											<CustomersIcon width={ 20 } height={ 20 } color={ ACCENT_COLOUR } />
+										</GridColumn>
+										{ !!customers?.length && (
+											<GridColumn style={{ flexGrow: 1 }}>
+												<LocationFilter placeholder="Filter by Customer" options={ customers.map( customer => ({ label: customer.customerName, value: customer?.id ?? '' })) } />
+											</GridColumn>
+										)}
+									</GridRow>
+								</GridColumn>
+								<GridColumn style={ styles.filtersColumn }>
+									<GridRow>
+										<GridColumn style={ styles.iconContainer }>
+											<SitesIcon width={ 20 } height={ 20 } color={ ACCENT_COLOUR } />
+										</GridColumn>
+									{ !!sites?.length && (
+										<GridColumn style={{ flexGrow: 1 }}>
+											<LocationFilter placeholder="Filter by Site" options={ sites.map( site => ({ label: site.siteName, value: site?.id ?? '' })) } />
+										</GridColumn>
+									)}
+									</GridRow>
+								</GridColumn>
+								<GridColumn style={ styles.filtersColumn }>
+									<GridRow>
+										<GridColumn style={ styles.iconContainer }>
+											<PlantsIcon width={ 20 } height={ 20 } color={ ACCENT_COLOUR } />
+										</GridColumn>
+										{ !!plants?.length && (
+											<GridColumn style={{ flexGrow: 1 }}>
+												<LocationFilter placeholder="Filter by Plant" options={ plants.map( plant => ({ label: plant.plantName, value: plant?.id ?? '' })) } />
+											</GridColumn>
+										)}
+									</GridRow>
+								</GridColumn>
+								<GridColumn style={ styles.filtersColumn }>
+									<GridRow>
+										<GridColumn style={ styles.iconContainer }>
+											<AreasIcon width={ 20 } height={ 20 } color={ ACCENT_COLOUR } />
+										</GridColumn>
+										{ !!areas?.length && (
+											<GridColumn style={{ flexGrow: 1 }}>
+												<LocationFilter placeholder="Filter by Area" options={ areas.map( area => ({ label: area.areaName, value: area?.id ?? '' })) } />
+											</GridColumn>
+										)}
+									</GridRow>
+								</GridColumn>
+								<GridColumn style={ styles.filtersColumn }>
+									<TouchableOpacityButton label="Apply Filters" pressHandler={() => {}} />
+								</GridColumn>
+							</GridRow>
+						</View>
 					</Card>
 				</CardsContainer>
 			</View>
@@ -87,10 +127,19 @@ export default function AssetTagsIndex() {
 				{ tags.map(( tag ) => (
 					<TouchableOpacity key={ tag.id } onPress={ () => router.push( `./asset-tags/${ tag.id }` ) }>
 						<Card>
-							<Text>Tag</Text>
-							<Text style={ styles.cardTitle }>
-								{ tag.name }
-							</Text>
+							{/* <Text>Tag</Text> */}
+							<View style={{ marginBottom: 30 }}>
+								<GridRow>
+									<GridColumn>
+										<View style={ styles.iconContainer }>
+											<AssetTagIcon width={ 20 } height={ 20 } />
+										</View>
+									</GridColumn>
+									<Text style={ styles.cardTitle }>
+										{ tag.name }
+									</Text>
+								</GridRow>
+							</View>
 							{ tag?.description && (
 								<Text style={ styles.cardDesc }>
 									{ tag.description }
@@ -128,7 +177,7 @@ const styles = StyleSheet.create({
 	cardTitle: {
 		fontSize: 18,
 		fontWeight: "600",
-		marginBottom: 20,
+		// marginBottom: 20,
 	},
 	cardDesc: {
 		fontSize: 14,
@@ -138,6 +187,14 @@ const styles = StyleSheet.create({
 		marginBottom: 30
 	},
 	filtersColumn: {
-		width: screenWidth > 768 ? '25%' : '100%',
+		width: screenWidth > 768 ? '20%' : '100%',
+		paddingInline: 5,
+	},
+	iconContainer: {
+		padding: 10,
+		borderWidth: 1,
+		borderRadius: "100%",
+		borderColor: ACCENT_COLOUR,
+		marginRight: 10
 	}
 });
