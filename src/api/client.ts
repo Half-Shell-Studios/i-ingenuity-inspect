bb3dff74d030ccd5efe1bf1dfa009366a8ff3ac7
Ethan Owen <ethan@eocreative.dev>
2026-06-10 18:33:15 +0100

    Clean up

--- src/api/client.ts
+++ src/api/client.ts
@@ -15,28 +15,17 @@ client.interceptors.request.use(async( config ) => {
 		config.headers.Authorization = `Bearer ${token}`;
 	}
 
-	// console.log(`${config.method?.toUpperCase()} ${config.baseURL}${config.url}`);
-	// console.log("Headers:", config.headers);
-	// console.log("Data:", config.data);
-
 	return config;
 });
 
 client.interceptors.response.use( response => {
-	// console.log("Headers:", response?.headers);
-	// console.log("Data:", response?.data);
-
 	return response;
 }, async( error ) => {
-	// console.log("Request failed:", error.message);
-    // console.log("Code:", error.code);
-    // console.log("Response:", error.response?.status, error.response?.data);
-	// console.log( "Error:", error );
 	if( error.response?.status === 401 ) {
 		await removeToken();
 	}
-	return Promise.reject( error );
-  },
+		return Promise.reject( error );
+	},
 );
 
 export default client;
