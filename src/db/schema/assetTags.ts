28606193a65b324f603295257f806505aaa686a0
Ethan Owen <ethan@eocreative.dev>
2026-06-10 18:40:00 +0100

    Explicity name faults relations

--- src/db/schema/assetTags.ts
+++ src/db/schema/assetTags.ts
@@ -30,8 +30,14 @@ export const assetTagsRelations = defineRelations(
 				references: [ locationsTable.id ],
 			}
 		),
-		faults: many(faultsTable),
-		faultsOpen: many(faultsTable),
-		faultsClosed: many(faultsTable),
+		faults: many(faultsTable, {
+			relationName: "assetTagAllFaults"
+		}),
+		faultsOpen: many(faultsTable, {
+			relationName: "assetTagOpenFaults"
+		}),
+		faultsClosed: many(faultsTable, {
+			relationName: "assetTagClosedFaults"
+		}),
 	})
 );
