e7bc0faa8b5d0a2766ff15dbe3f7bbc9e93ba224
Ethan Owen <ethan@eocreative.dev>
2026-06-17 23:39:40 +0100

    Add optional faultCode

--- src/types/Fault.ts
+++ src/types/Fault.ts
@@ -1,19 +1,23 @@
 import AssetTag from "./AssetTag";
+import { FaultCode } from "./FaultCode";
+import Inspection from "./Inspection";
 
 export interface Fault {
-	id: string,
-	locationId: string,
-	assetTagId: string,
-	inspectionId: string,
-	faultCodeId: string,
-	section: string,
-	question: string,
-	raisedAt: string,
-	raisedComment: string,
-	closedAt: string | null,
-	closedBy: string | null,
-	closedComment: string | null,
-	assetTag?: AssetTag
+	id: string;
+	locationId: string;
+	assetTagId: string;
+	inspectionId: string;
+	faultCodeId: string;
+	section: string;
+	question: string;
+	raisedAt: string;
+	raisedComment: string;
+	closedAt: string | null;
+	closedBy: string | null;
+	closedComment: string | null;
+	assetTag?: AssetTag;
+	inspection?: Inspection;
+	faultCode?: FaultCode;
 }
 
 export interface FaultSection {
