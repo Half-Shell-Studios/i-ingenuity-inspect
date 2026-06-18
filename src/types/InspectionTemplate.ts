b43c4b79c72f57fb4555eff0bc2fd44db4fc57d6
Ethan Owen <ethan@eocreative.dev>
2026-06-18 19:02:43 +0100

    Fix type definitions

--- src/types/InspectionTemplate.ts
+++ src/types/InspectionTemplate.ts
@@ -1,18 +1,18 @@
 export interface InspectionTemplate {
-	id: string,
-	revisionActiveId: string,
-	revisionActive?: InspectionTemplateRevision,
-	revisionLatestId: string
+	id: string;
+	revisionActiveId: string;
+	revisionActive?: InspectionTemplateRevision;
+	revisionLatestId: string;
 }
 
 export interface InspectionTemplateRevision {
-	id: string,
-	inspectionTemplateId: string,
-	inspectionTypeId: string,
-	name: string
-	description: string | null,
-	criteria: string | null,
-	template: string
+	id: string;
+	inspectionTemplateId: string;
+	inspectionTypeId: string;
+	name: string;
+	description: string | null;
+	criteria: string | null;
+	template: string;
 }
 
 export default InspectionTemplate
