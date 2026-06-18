21f0b05dba1de62ffceeab282aa7625cb9aa3d3c
Ethan Owen <ethan@eocreative.dev>
2026-06-18 19:01:41 +0100

    Add inspection type and inspection template to type definition

--- src/types/Inspection.ts
+++ src/types/Inspection.ts
@@ -1,3 +1,6 @@
+import InspectionTemplate from "./InspectionTemplate";
+import InspectionType from "./InspectionType";
+
 type InspectionAnswer = {
 	pass: boolean;
 	notes: string;
@@ -6,23 +9,25 @@ type InspectionAnswer = {
 };
 
 export interface InspectionAnswers {
-	answer: InspectionAnswer,
-	skipped: boolean
+	answer: InspectionAnswer;
+	skipped: boolean;
 }
 
 export interface Inspection {
-	id: string,
-	assetTagId: string,
-	assetTemplateId: string,
-	assetTemplateRevisionId: string,
-	inspectionTemplateId: string,
-	inspectionTemplateRevisionId: string,
-	inspectionTypeId: string,
-	locationId: string,
-	name: string,
-	assessment: InspectionAnswers[],
-	notes: string | null,
-	inspectedBy: string
+	id: string;
+	assetTagId: string;
+	assetTemplateId: string;
+	assetTemplateRevisionId: string;
+	inspectionTemplateId: string;
+	inspectionTemplateRevisionId: string;
+	inspectionTypeId: string;
+	locationId: string;
+	name: string;
+	assessment: InspectionAnswers[];
+	notes: string | null;
+	inspectedBy: string;
+	inspectionTemplate: InspectionTemplate;
+	inspectionType: InspectionType;
 }
 
 export default Inspection
