fb08dc29779199bb058b3c2a3423757770ff685e
Ethan Owen <ethan@eocreative.dev>
2026-07-13 08:23:33 +0100

    Handle for empty UUID value

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -3,8 +3,9 @@ import type { User } from '@/src/types';
 import { eq } from "drizzle-orm";
 import { AppDatabase } from "..";
 
-export async function getUser( db: AppDatabase | null, userId: User[ 'id' ] ) {
+export async function getUser( db: AppDatabase | null, userId: User[ 'id' ] | undefined ) {
 	if( !db ) throw new Error( 'Databse not initialised' );
+	if( !userId ) throw new Error( 'A valid User ID is required.' );
 
 	return db.query.usersTable.findFirst({
 		where: eq( usersTable.id, userId )
