5c6ecb5b1f3db5c100d2c9ba06683ad17f1a7486
Ethan Owen <ethan@eocreative.dev>
2026-07-15 16:04:41 +0100

    Create a standardised body text colour. Create a standardised text input placeholder colour.

--- src/constants/colours.ts
+++ src/constants/colours.ts
@@ -14,4 +14,7 @@ export const PRIMARY_COLOUR = BRAND_COLOUR_BLUE;
 export const SECONDARY_COLOUR = BRAND_COLOUR_YELLOW;
 export const ACCENT_COLOUR = BRAND_COLOUR_PURPLE;
 
+export const BODY_TEXT_COLOUR = BRAND_COLOUR_NAVY;
+export const PLACEHOLDER_TEXT_COLOUR = '#9ca3af';
+
 export const darkModeActive = ( Appearance.getColorScheme() === 'dark' );
